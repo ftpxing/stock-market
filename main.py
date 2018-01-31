f58fe7a2d87ce1ab2ee3340692b27e06c0d64dc0
xing <575950911@qq.com>
2018-01-31 16:41:00 +0800

    1.0

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 import easyquotation
 import time as tt
 import logging as log
+import requests
 
 # 命名py脚本时，不要与python预留字，模块名等相同
 # 否则会报错AttributeError: module 'easyquotation' has no attribute 'PY_VERSION'
-
 # logging模块配置 https://www.cnblogs.com/yyds/p/6901864.html
-LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-log.basicConfig(filename='common.log', level=log.DEBUG, format=LOG_FORMAT)
-quotation = easyquotation.use("sina")
-# quotation.market_snapshot(prefix=True)
-平安银行 = quotation.real('000001')
-detail = 平安银行.get('000001')
-log.info("this is info log")
-log.error("this is error log")
+# 股市行情获取API https://github.com/shidenggui/easyquotation
+LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+log.basicConfig(filename='common.log', level=log.INFO, format=LOG_FORMAT)
+apiKey = 'c457e4828891683f47ec6ae646330dbe'
+mobile = '15613105771'
+sms_api = 'https://sms.yunpian.com/v2/sms/single_send.json'
 
-while 1:
-    name = detail.get('name')
-    time = detail.get('time')
-    high = detail.get('high')
-    now = detail.get('now')
-    open = detail.get('open')
-    low = detail.get('low')
-    print(name+'----'+time)
-    print('开盘：'+str(open))
-    print('今日最高：'+str(high))
-    print('今日最低：'+str(low))
-    print('当前：'+str(now))
-    print((now-high)/high)
-    tt.sleep(10)
+
+def cycle_check():
+    quotation = easyquotation.use('sina')
+    # 000651格力电器 000040东旭蓝天 002230科大讯飞
+    codes = ['000651', '000040', '002230']
+    while 1:
+        sys_time = tt.strftime("%H:%M:%S", tt.localtime())
+        for code in codes:
+            detail = quotation.real(code).get(code)
+            name = detail.get('name')
+            high = detail.get('high')
+            low = detail.get('low')
+            time = detail.get('time')
+            if (high-low)/high >= 0.03:
+                log.error(name+code+'下跌了，发送预警短信')
+                send_message(name=name, code=code)
+                codes.remove(code)
+                log.info(name+code+'被移出关注列表')
+        if sys_time > time :
+            log.info('<-- 交易关闭了，停止监控')
+            break
+        tt.sleep(5)
+
+
+def send_message(name, code):
+    text = '【星空科技】名称'+name+' 代码'+code+'价格异常，请及时处理'
+    param = {'apikey':apiKey,'text':text,'mobile':mobile}
+    headers = {'user-agent': 'my-app/0.0.1','Content-Type':'application/x-www-form-urlencoded'}
+    resp = requests.post(url=sms_api,data=param,headers=headers)
+    log.info('调用云片短信API回执:'+resp.text)
+
+if __name__ == '__main__':
+    log.info('开始监控 -->')
+    cycle_check()
